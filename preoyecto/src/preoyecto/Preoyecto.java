97289cbfc90ecf5e70aedcdfc3e492f1dce06525
Alvaro <alvarodiazargamasilla@gmail.com>
2025-11-11 17:31:56 +0100

    dos commit

--- preoyecto/src/preoyecto/Preoyecto.java
+++ preoyecto/src/preoyecto/Preoyecto.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
+
+
 package preoyecto;
 
-/**
- *
- * @author alvaro
- */
 public class Preoyecto {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("buena madrugada");
+        System.out.println("malas madrugada");
+        System.out.println("papapapapapa");
+        int a = 1;
     }
     
 }
